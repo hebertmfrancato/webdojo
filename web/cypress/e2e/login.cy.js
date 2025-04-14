decfde75be203782833641a23e73fc971bd1251c
Hebert Francato <hebertmfrancato@gmail.com>
2025-04-13 22:29:33 -0300

    Adicona cenário de senha incorreta e e-mail não cadastrado

--- web/cypress/e2e/login.cy.js
+++ web/cypress/e2e/login.cy.js
@@ -19,8 +19,46 @@ describe('Login', () => {
       .should('be.visible')
       .and('have.text', 'Fernando Papito')
 
-      cy.get('[data-cy="welcome-message"]')
+    cy.get('[data-cy="welcome-message"]')
       .should('be.visible')
       .and('have.text', 'Olá QA, esse é o seu Dojo para aprender Automação de Testes.')
+  })
+
+  it('Não deve logar com senha incorreta', () => {
+    cy.viewport(1440, 900)
+    cy.visit('http://localhost:3000')
+
+    cy.get('#email')
+      .should('be.visible')
+      .type('papito@webdojo.com')
+    cy.get('#password')
+      .should('be.visible')
+      .type('katana321')
+
+    cy.contains('button', 'Entrar')
+      .should('be.visible')
+      .click()
+
+    cy.contains('Acesso negado! Tente novamente.')
+      .should('be.visible')
+  })
+
+  it('Não deve logar com e-mail não cadastrado', () => {
+    cy.viewport(1440, 900)
+    cy.visit('http://localhost:3000')
+
+    cy.get('#email')
+      .should('be.visible')
+      .type('teste@webdojo.com')
+    cy.get('#password')
+      .should('be.visible')
+      .type('katana123')
+
+    cy.contains('button', 'Entrar')
+      .should('be.visible')
+      .click()
+
+    cy.contains('Acesso negado! Tente novamente.')
+      .should('be.visible')
   })
 })
